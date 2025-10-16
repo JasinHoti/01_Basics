7e98a9f15bb083e636dca7efad172dae1e14f312
Jasin <jasin@example.com>
2025-10-16 12:54:03 +0200

    Test 2

--- js5.js
+++ js5.js
@@ -13,5 +13,3 @@ let result2 = Math.round((bananegewicht * 17 * bananaPricePerKilo) * 100) / 100;
 console.log("Preis von 8 Äpfeln ist: " + result1 + " €");
 console.log("Preis von 17 Bananen ist: " + result2 + " €");
 console.log("hello")
-console.log("test")
-console.log("testn2")
