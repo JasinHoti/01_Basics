285078ccb1e08995854c12d74fac9cb32fbc4b69
Jasin <jasin@example.com>
2025-11-06 12:26:53 +0100

    10 JS Variablen & Operationen Done

--- 10_JS_Variablen/js1.js
+++ 10_JS_Variablen/js1.js
@@ -3,9 +3,17 @@ console.log("Tolle Sache");
 let a = 7;
 let b = 8;
 let c = 10;
-let result = (a + b + c) / 4;
-console.log(result);
+let result1 = a + b + c
+let result2 = (a +b) / c 
+let result3 = c -a + b 
+let result4 = a + b + c * 3 
+let result5 = (a + b + c) / 4;
 
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
 
 
 
--- 20_JS_Strings_Zahlen/js5.js
+++ 20_JS_Strings_Zahlen/js5.js
@@ -4,13 +4,16 @@ let applePricePerKilo = 3.43;
 
 let apfelgewicht = 0.34;
 let bananegewicht = 0.22;
+let bananaPricePerTon = (bananaPricePerKilo * 1000)
+let applePricePeKilo = (applePricePeKilo * 1000)
 
 // Preisberechnung
-let result1 = Math.round((apfelgewicht * 8 * applePricePerKilo) * 100) / 100;
-let result2 = Math.round((bananegewicht * 17 * bananaPricePerKilo) * 100) / 100;
+let result1 = (apfelgewicht * 8 * applePricePerKilo) 
+let result2 = (bananegewicht * 17 * bananaPricePerKilo)
+let result3 = (apfelgewicht * 17 * bananaPricePerKilo)
 
+console.log("Preis von 17 Bananen ist: " + result2 + " €");
 // Ausgabe
 console.log("Preis von 8 Äpfeln ist: " + result1 + " €");
-console.log("Preis von 17 Bananen ist: " + result2 + " €");
+console.log("Preis pro Tonne Bananen: " + bananaPricePerTon + " €");
 
-//  terst
