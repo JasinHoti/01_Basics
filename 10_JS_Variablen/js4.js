1c8e73def1fedd9c88e0bedec4a97c5941dabf94
Jasin <jasin@example.com>
2025-11-06 12:39:23 +0100

    20 JS Strings & Zahlen

--- 10_JS_Variablen/js4.js
+++ 10_JS_Variablen/js4.js
@@ -1,10 +1,13 @@
-let a = 7;
-let b = 4.4;
+let a = 17.4;
+let b = 3.9;
 
-let result = a + b;
+let result = a / b
+
+console.log("The result of 17.4 divided by 3.9 is" + result)
+
 
 //string
 let fistname = "Jasin";
 let lastname = "Hoti";
-console.log(fistname + " " + lastname + " " + result); 
+console.log(fistname + " " + lastname + " " + result);
 
