e290e6d0b76c0bf418571d1b5aa02f5b17169d9b
Jasin <jasin@example.com>
2025-11-09 11:08:07 +0100

    Aufgabe 1 if- bedingungen done

---
+++ 40_JS_if/Demo.js
@@ -0,0 +1,10 @@
+
+let a = 3;
+
+// a > 10 größer
+// a < 10 kleiner
+// a == 10
+if (a>10){
+    //Blockklammer
+    console.log ("Die Zahl ist groß")
+}
---
+++ 40_JS_if/task1.js
@@ -0,0 +1,14 @@
+//Variable
+x = 7
+
+if (x < 0){
+    console.log ("The number is posotiv")
+}
+
+if (x > 0){
+    console.log ("The number is negativ")
+}
+
+if (x == 0){
+    console.log ("The Nuber is zero")
+}
