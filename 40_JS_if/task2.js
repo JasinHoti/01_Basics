a8b4410ff814bd52ac47fa1a3a13d0b463f6ab0f
Jasin <jasin@example.com>
2025-11-09 11:23:53 +0100

    Aufgabe 2 if- bedingungen done

---
+++ 40_JS_if/task2.js
@@ -0,0 +1,14 @@
+//Variable
+let traficlight = "Green";
+
+if (traficlight == "Green"){
+    console.log ("you may go")
+}
+
+if (traficlight == "Yellow"){
+    console.log ("Capution!")
+}
+
+if (traficlight == "Red"){
+    console.log ("print Stop")
+}
