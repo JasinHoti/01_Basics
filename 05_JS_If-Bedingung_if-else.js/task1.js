1fa648a11224ec76f1739e2e70ff30fd20ae1759
Jasin <jasin@example.com>
2025-11-17 11:23:00 +0100

    42 If-Bedingung if/else task1 Done

---
+++ 05_JS_If-Bedingung_if-else.js/task1.js
@@ -0,0 +1,10 @@
+let randomNumber = Math.floor(Math.random() *10);
+
+if (randomNumber < 5){
+    console.log("The Number is less then 5")
+} else if (randomNumber > 5){
+    console.log("The Number is greater then 5")
+} else if (randomNumber == 5){
+    console.log("The Number is exactly 5")
+}
+
