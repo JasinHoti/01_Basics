6e39ecb1e634a66686d070a1ad01003aefcddaec
Jasin <jasin@example.com>
2025-11-17 11:04:17 +0100

    42 If-Bedingung if/else if3 Done

---
+++ 05_JS_If-Bedingung_if-else.js/Demo2.js
@@ -0,0 +1,17 @@
+let a = Math.floor(Math.random() *100);
+
+console.log(a);
+
+// a > 10   größer
+// a < 10   kleiner
+// a == 7   gleich
+// a >= 10  größer gleich
+// a <= 10  kleiner gleich
+
+if (a >= 80) {
+  console.log("Die Zahl ist groß");
+} else if (a >= 40) {
+  console.log("Die Zahl ist positiv aber klein");
+} else {
+  console.log("Die Zahl ist klein");
+}
