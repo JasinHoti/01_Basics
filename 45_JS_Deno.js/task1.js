3e55651b9652d954f83b75db9835c576928b3dc0
Jasin <jasin@example.com>
2025-12-04 12:40:40 +0100

    Deno Task1 Done

---
+++ 45_JS_Deno.js/task1.js
@@ -0,0 +1,8 @@
+let userName = prompt("Benutzername eingeben");
+let password = prompt("Gib das Passwort ein");
+
+if (userName === "Jasin_Hoti" && password === "HTL") {
+  console.log("Correct");
+} else {
+  console.log("faslch");
+}
