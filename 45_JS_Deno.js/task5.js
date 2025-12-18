10e4b380f7b24282d6430037cedbc092c36eed35
Jasin <jasin@example.com>
2025-12-18 11:50:21 +0100

    Deno task6 done

--- 45_JS_Deno.js/task5.js
+++ 45_JS_Deno.js/task5.js
@@ -1,17 +1,17 @@
 let temperatur = prompt("Gib die heutige Temperatur ein");
 
-let temperaturAsNumber = parseInt(temperatur);
+let selection = parseInt(temperatur);
 
 let result = 0;
 
-if (temperaturAsNumber <= -3) {
+if (selection <= -3) {
   console.log("Stay at Home, its to colddd");
-} else if (temperaturAsNumber < 18 && temperaturAsNumber >= -2) {
+} else if (selection < 18 && selection >= -2) {
   console.log("You have to ziehen you warm an, sonst your freezing");
-} else if (temperaturAsNumber < 28 && temperaturAsNumber > 19) {
+} else if (selection < 28 && selection > 19) {
   console.log("Go out of your hous, touch grasss");
-}else if (temperaturAsNumber < 40 && temperaturAsNumber >= 29){
-    console.log("Go swim its very hot")
-}else if (temperaturAsNumber >= 41){
-    console.log("Put some ice on your head")
+} else if (selection < 40 && selection >= 29) {
+  console.log("Go swim its very hot");
+} else if (selection >= 41) {
+  console.log("Put some ice on your head");
 }
---
+++ 45_JS_Deno.js/task6.js
@@ -0,0 +1,17 @@
+let input = prompt("Welches Getränk möchtest du, Cola, Wasser, Bier oder Tee?");
+
+console.log("input",input);
+
+
+let result = 0;
+
+if (input == "Cola") {
+  console.log("Danke für deine Bestellung von Cola.");
+} if (input == "Wasser") {
+  console.log("Danke für deine Bestellung von Wasser.");
+} if (input == "Bier") {
+  console.log("Danke für deine Bestellung von Bier." );
+} if (input == "Tee") {
+  console.log("Danke für deine Bestellung von Tee." );
+} 
+
