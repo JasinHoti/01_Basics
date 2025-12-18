62cef7c6437441b555332fd7c3b4beb1bf0cc296
Jasin <jasin@example.com>
2025-12-18 12:47:30 +0100

    50 Logische Operatoren task1 done

---
+++ 50_JS_LogischeOperatoren.js/task1.js
@@ -0,0 +1,17 @@
+let grades = prompt("Wieviel Prozent hast du in deinem Test?");
+
+let gradesAsNumber = Number(grades);
+console.log(gradesAsNumber);
+
+let result = 0;
+
+if (gradesAsNumber == 90 || gradesAsNumber > 90) {
+  console.log("You have an A!");
+}
+if (gradesAsNumber == 80 || gradesAsNumber > 80 && gradesAsNumber < 89 || gradesAsNumber == 89){
+    console.log("You have an B!")
+}if (gradesAsNumber == 70 || gradesAsNumber > 70 && gradesAsNumber < 79 || gradesAsNumber == 79){
+    console.log("You have an C!")
+}if (gradesAsNumber == 60 || gradesAsNumber > 60 && gradesAsNumber < 69 || gradesAsNumber == 69){
+    console.log("You have an D!")
+} else console.log("You have a F!")
