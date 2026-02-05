f0cf82ba852d3dd617a15de70e98e00d4969aef6
Jasin <jasin@example.com>
2026-02-05 12:06:46 +0100

    REORG

--- 01_JS/110_Allays_js/ExtraAufgabe_Noten.js
+++ 01_JS/110_Allays_js/ExtraAufgabe_Noten.js
@@ -1,8 +1,10 @@
 let sum = 0
 
 let input1 = Number(prompt("Gib deine Anzahl an Noten ein"));
+let input2 = Number(prompt("Gib die erste note ein"))
 let arr = [ ]
+
 arr.push(input1)
-for(let i = 0; i < arr.length; i++){
-sum = sum + arr[i]
+for(let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i]
 } console.log(sum / arr.length)
