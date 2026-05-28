82fe60de33a8f3d9e7ccb012a184a658a3e8a5fb
Jasin <jasin@example.com>
2026-05-28 12:55:37 +0200

    demo finish

--- 03_WebandJS/2demo.js
+++ 03_WebandJS/2demo.js
@@ -1,4 +1,11 @@
 document.getElementById("button").addEventListener("click", function () {
-  const element = document.getElementById("content");
-  element.innerHTML = "Super";
+  const vaule1 = document.getElementById("vaule1").value;
+  const vaule2 = document.getElementById("vaule2").value;
+
+  if (isNaN(vaule1) || isNaN(vaule2)) {
+    alert("Invalid value");
+  } else {
+    const sum = parseFloat(vaule1) + parseFloat(vaule2);
+    document.getElementById("result").innerText = "Sum: " + sum;
+  }
 });
