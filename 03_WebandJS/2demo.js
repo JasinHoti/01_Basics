1596d845d62d654774a33dedf710ec29eeb4608d
Jasin <jasin@example.com>
2026-06-18 12:47:20 +0200

    Added Calculator

--- 03_WebandJS/2demo.js
+++ 03_WebandJS/2demo.js
@@ -1,5 +1,5 @@
-document.getElementById("button").addEventListener("click", function () {
-  const vaule1 = document.getElementById("vaule1").value;
+let button = document.getElementById("button").addEventListener("click", function () {
+  let vaule1 = document.getElementById("vaule1").value;
   const vaule2 = document.getElementById("vaule2").value;
 
   if (isNaN(vaule1) || isNaN(vaule2)) {
@@ -9,3 +9,39 @@ document.getElementById("button").addEventListener("click", function () {
     document.getElementById("result").innerText = "Sum: " + sum;
   }
 });
+
+document.getElementById("button2").addEventListener("click", function () {
+  const vaule1 = document.getElementById("vaule1").value;
+  const vaule2 = document.getElementById("vaule2").value;
+
+  if (isNaN(vaule1) || isNaN(vaule2)) {
+    alert("Invalid value");
+  } else {
+    const sum = parseFloat(vaule1) - parseFloat(vaule2);
+    document.getElementById("result2").innerText = "Minus: " + sum;
+  }
+});
+
+document.getElementById("button3").addEventListener("click", function () {
+  const vaule1 = document.getElementById("vaule1").value;
+  const vaule2 = document.getElementById("vaule2").value;
+
+  if (isNaN(vaule1) || isNaN(vaule2)) {
+    alert("Invalid value");
+  } else {
+    const sum = parseFloat(vaule1) * parseFloat(vaule2);
+    document.getElementById("result3").innerText = "Multiplicate: " + sum;
+  }
+});
+
+document.getElementById("button4").addEventListener("click", function () {
+  const vaule1 = document.getElementById("vaule1").value;
+  const vaule2 = document.getElementById("vaule2").value;
+
+  if (isNaN(vaule1) || isNaN(vaule2)) {
+    alert("Invalid value");
+  } else {
+    const sum = parseFloat(vaule1) / parseFloat(vaule2);
+    document.getElementById("result4").innerText = "Divid: " + sum;
+  }
+});  
